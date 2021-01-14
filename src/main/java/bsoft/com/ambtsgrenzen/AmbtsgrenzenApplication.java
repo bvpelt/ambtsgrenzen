94c0ccef1f8f44578cd097511cff5e45bb86a7f7
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2021-01-14 16:56:31 +0100

    Initial commit

---
+++ src/main/java/bsoft/com/ambtsgrenzen/AmbtsgrenzenApplication.java
@@ -0,0 +1,13 @@
+package bsoft.com.ambtsgrenzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbtsgrenzenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbtsgrenzenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bsoft/com/ambtsgrenzen/AmbtsgrenzenApplicationTests.java
@@ -0,0 +1,13 @@
+package bsoft.com.ambtsgrenzen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbtsgrenzenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

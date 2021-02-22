5534551339eac509cc94824f6e98ce18c9a0857a
bart-van-pelt <66597958+bart-van-pelt@users.noreply.github.com>
2021-02-22 15:23:02 +0100

    Update geoqueries.sql

--- docs/geoqueries.sql
+++ docs/geoqueries.sql
@@ -119,3 +119,99 @@ where b1.openbaarlichaam_id = o1.id and
     ST_intersects(b1.geometry, b2.geometry) and
         b1.id = 501 and
         b1.id <> b2.id;
+                            
+                           
+-- Testsets
+--                          
+delete from bestuurlijkgebied where id > 390;
+delete from openbaarlichaam where id > 390;
+
+-- toon alle gemeenten in nederland
+select b.*, o.* from bestuurlijkgebied b, openbaarlichaam o where b.openbaarlichaam_id = o.id and b.id < 390 and b.identificatie like 'GM%';
+
+-- toon gemeente zaltbommel
+select b.*, o.* from bestuurlijkgebied b, openbaarlichaam o where b.openbaarlichaam_id = o.id and o.name = 'Zaltbommel';
+
+--
+-- bepaal welke bestuurlijke gezagen een intersect hebben met gemeente Zaltbommel
+--
+select 'bron', b1.id, b1.identificatie, o1.name, 'intersects', b2.id, b2.identificatie, o2.name, b2.geometry
+from bestuurlijkgebied b1, openbaarlichaam o1, bestuurlijkgebied b2, openbaarlichaam o2
+where b1.openbaarlichaam_id = o1.id and
+        b2.openbaarlichaam_id = o2.id and
+    ST_intersects(b1.geometry, b2.geometry) and
+        o1.name = 'Zaltbommel' and
+        b1.id <> b2.id and
+		b2.id < 390;
+		
+-- 
+-- normale dataset ids 0..389
+--
+-- id 400 - gemeente zaltbommel -1 meter
+-- id 500 - perceel 500 aan de rand van zaltbommel
+-- id 600 - perceel 500 -1 meter
+
+--
+-- insert gebied zaltbommel met verkleinde geometry met id 400
+--
+delete from openbaarlichaam where id = 400;
+delete from bestuurlijkgebied where id = 400;
+insert into openbaarlichaam (id, code, type, name) values (400, 'buffer -1', 'Gemeente', 'Zaltbommel -1');
+insert into bestuurlijkgebied (id, identificatie, domein, type, openbaarlichaam_id, geometry )
+select 400, 'buffer -1', 'NL.BI.BestuurlijkGebied', 'territoriaal', 400, ST_buffer(x.geometry, -0.001) from bestuurlijkgebied x where id = 79;
+
+select b.*, o.* from bestuurlijkgebied b, openbaarlichaam o where b.openbaarlichaam_id = o.id and b.id in (79,400);
+--
+-- bepaal welke bestuurlijke gezagen een intersect hebben met gemeente Zaltbommel -1
+--
+select 'bron', b1.id, b1.identificatie, o1.name, 'intersects', b2.id, b2.identificatie, o2.name, b2.geometry
+from bestuurlijkgebied b1, openbaarlichaam o1, bestuurlijkgebied b2, openbaarlichaam o2
+where b1.openbaarlichaam_id = o1.id and
+        b2.openbaarlichaam_id = o2.id and
+    ST_intersects(b1.geometry, b2.geometry) and
+        o1.name = 'Zaltbommel -1' and
+        b1.id <> b2.id and
+		b2.id < 500;
+
+--
+-- Via de viewer bepaald dat perceel KWK02 L 274 op de grens van Zaltbommel ligt
+-- Insert Perceel op de grens van Zaltbommel met id 500
+--
+insert into openbaarlichaam (id, code, type, name) values (500, 'perceel 500', 'Perceel', 'perceel 500');
+
+insert into bestuurlijkgebied (id, identificatie, domein, type, openbaarlichaam_id, geometry )
+values (500, 'KWK02 L 274', 'Perceel', 'territoriaal', 500,
+        ST_GeometryFromText('POLYGON((141019.113 417056.301, 141082.863 417041.791, 141073.677 417132.101, 141036.451 417138.578, 141007.173 417145.909, 140990.686 417149.43, 140950.198 417157.788, 140918.127 417164.489, 140879.743 417172.47, 140875.637 417173.321, 140845.368 417179.595, 140773.109 417194.658, 140742.941 417201.129, 140700.997 417210.022, 140642.339 417220.331, 140592.044 417229.332, 140569.112 417232.382, 140522.801 417238.226, 140468.28 417245.079, 140431.951 417247.982, 140352.436 417254.311, 140341.725 417254.854, 140288.404 417257.559, 140211.015 417261.461, 140164.692 417263.853, 140069.217 417261.904, 140004.078 417261.139, 139946.743 417259.959, 139893.429 417256.872, 139841.767 417253.554, 139752.123 417244.016, 139640.799 417237.102, 139634.71 417147.395, 139699.913 417157.357, 139799.993 417167.13, 139845.207 417170.049, 139900.962 417173.475, 139999.264 417175.247, 140099.599 417175.242, 140115.856 417174.987, 140200.188 417172.316, 140300.142 417168.12, 140331.875 417166.435, 140400.615 417161.99, 140500.279 417154.713, 140599.064 417142.884, 140699.548 417127.962, 140800.013 417107.323, 140817.184 417103.195, 141019.113 417056.301))', 28992));
+
+select b.*, o.* from bestuurlijkgebied b, openbaarlichaam o where b.openbaarlichaam_id = o.id and b.id in (79,500);
+
+--
+-- Welke intersect is er met perceel 500
+--
+select 'bron', b1.id, b1.identificatie, o1.name, 'intersects', b2.id, b2.identificatie, o2.name, b2.geometry
+from bestuurlijkgebied b1, openbaarlichaam o1, bestuurlijkgebied b2, openbaarlichaam o2
+where b1.openbaarlichaam_id = o1.id and
+        b2.openbaarlichaam_id = o2.id and
+    ST_intersects(b1.geometry, b2.geometry) and
+        o1.name = 'perceel 500' and
+        b1.id <> b2.id and
+		b2.id NOT in (400, 600);
+		
+--
+-- Insert perceel 500 -1 meter met id 600
+--
+insert into openbaarlichaam (id, code, type, name) values (600, 'perc -1', 'Perceel', 'Perceel 500 -1');
+insert into bestuurlijkgebied (id, identificatie, domein, type, openbaarlichaam_id, geometry )
+select 600, 'perc -1', 'NL.BI.BestuurlijkGebied', 'territoriaal', 600, ST_buffer(x.geometry, -1) from bestuurlijkgebied x where id = 500;
+
+select b.*, o.* from bestuurlijkgebied b, openbaarlichaam o where b.openbaarlichaam_id = o.id and b.id in (500,600);
+
+select 'bron', b1.id, b1.identificatie, o1.name, 'intersects', b2.id, b2.identificatie, o2.name, b2.geometry
+from bestuurlijkgebied b1, openbaarlichaam o1, bestuurlijkgebied b2, openbaarlichaam o2
+where b1.openbaarlichaam_id = o1.id and
+        b2.openbaarlichaam_id = o2.id and
+    ST_intersects(b1.geometry, b2.geometry) and
+        o1.name = 'Perceel 500 -1' and
+        b1.id <> b2.id and
+		b2.id NOT in (400, 500);                            
+                            

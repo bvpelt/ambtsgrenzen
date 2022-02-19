9c8bc61947e5d87eccac17e64730d16f749885bc
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2022-02-19 20:32:25 +0100

    genapi

--- src/main/java/bsoft/com/ambtsgrenzen/AmbtsgrenzenApplication.java
+++ src/main/java/bsoft/com/ambtsgrenzen/AmbtsgrenzenApplication.java
@@ -1,7 +1,9 @@
 package bsoft.com.ambtsgrenzen;
 
+import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
